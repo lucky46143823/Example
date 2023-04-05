4bf41d64c9c6954f54a91234693c6d7ee3f9b9fe
lucky46143823 <129898804+lucky46143823@users.noreply.github.com>
2023-04-05 13:54:43 +0200

    Add files via upload

---
+++ SU5_1_46143823.py
@@ -0,0 +1,18 @@
+#Enter your nick name and your hobby
+nick_name = input("Enter your nickname : ")
+your_hobby = input("Enter your hobby : ")
+print()
+
+print("Your nickname is ",nick_name, " and your hobby is ",your_hobby, "!")
+print()
+#Enter a character
+_character = input("Enter a character: ")
+print()
+#print steps
+print("Steps")
+#Drawing a shape from your character
+print(_character)
+print(_character,_character)
+print(_character,_character,_character)
+print(_character,_character,_character,_character)
+print(_character,_character,_character,_character,_character)
